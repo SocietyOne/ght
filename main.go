36426beb93e689c15638d0d8468bbf3118ff5c5b
James Prendergast <james.prendergast@societyone.com.au>
2018-08-16 12:42:45 +1000

    formatting (mixed spaces and tabs)

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "os"
-  "fmt"
-	"log"
-  "io/ioutil"
-  "strings"
-	"html/template"
-	"net/http"
+	"fmt"
+	"github.com/google/go-github/github"
 	"github.com/sfreiberg/gotwilio"
-  "github.com/google/go-github/github"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 // According to the test instructions, format messages like so:
@@ -24,24 +24,24 @@ const smsTemplate = `Issue {{.Title}} {{.Url}}
 // Handle requests made to our webhook
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
 
-  smsNumber, ok := os.LookupEnv("TWILIO_SMS_NUMBER")
-  if !ok {
-    log.Fatal("Missing required environment variable TWILIO_SMS_NUMBER: set this to the recipient verified mobile number!")
-  }
+	smsNumber, ok := os.LookupEnv("TWILIO_SMS_NUMBER")
+	if !ok {
+		log.Fatal("Missing required environment variable TWILIO_SMS_NUMBER: set this to the recipient verified mobile number!")
+	}
 
-  // Twilio integration - account keys
-  // Note: these are my test credentials, for my account jacqui.maher@gmail.com
-  // https://www.twilio.com/console/project/settings
-  // twilioAccountSid := "AC4e4f8c6c42a699f065badd25f5137a00"
-  // twilioAuthToken := "c7ff639a05b9e03b57222a3b212364d8"
-  // TODO: for a real app I wouldn't hardcode any of this, refactor if actually using...
-  twilioAccountSid := "AC6f89947b675cb7faff5ca54001a888fb"
-  twilioAuthToken := "19b3029e50a1dcdba16119fa8c69ef73"
+	// Twilio integration - account keys
+	// Note: these are my test credentials, for my account jacqui.maher@gmail.com
+	// https://www.twilio.com/console/project/settings
+	// twilioAccountSid := "AC4e4f8c6c42a699f065badd25f5137a00"
+	// twilioAuthToken := "c7ff639a05b9e03b57222a3b212364d8"
+	// TODO: for a real app I wouldn't hardcode any of this, refactor if actually using...
+	twilioAccountSid := "AC6f89947b675cb7faff5ca54001a888fb"
+	twilioAuthToken := "19b3029e50a1dcdba16119fa8c69ef73"
 
-  // Create a new client able to talk to Twilio's API
+	// Create a new client able to talk to Twilio's API
 	twilio := gotwilio.NewTwilioClient(twilioAccountSid, twilioAuthToken)
 
-  // Read in the request body and make sure we can parse it first:
+	// Read in the request body and make sure we can parse it first:
 	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println("Unable to read payload body: ", err)
@@ -55,35 +55,35 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-  // Although I setup this webhook to only fire on Issue-related events,
-  // we still check that it's a github.IssuesEvent type
-  switch e := event.(type) {
-  case *github.IssuesEvent:
-    fmt.Println("Received issues event from GitHub with action ", *e.Action)
+	// Although I setup this webhook to only fire on Issue-related events,
+	// we still check that it's a github.IssuesEvent type
+	switch e := event.(type) {
+	case *github.IssuesEvent:
+		fmt.Println("Received issues event from GitHub with action ", *e.Action)
 
-    // Populate data to fill in that sms template created at the beginning:
-    templateData := map[string]interface{}{
-      "Title":    *e.Issue.Title,
-      "Url":      *e.Issue.URL,
-      "Action":   *e.Action,
-      "Username": *e.Sender.Login,
-      "Repo":     *e.Repo.Name,
-    }
+		// Populate data to fill in that sms template created at the beginning:
+		templateData := map[string]interface{}{
+			"Title":    *e.Issue.Title,
+			"Url":      *e.Issue.URL,
+			"Action":   *e.Action,
+			"Username": *e.Sender.Login,
+			"Repo":     *e.Repo.Name,
+		}
 
-    // parse the data into the template string:
-    t := template.Must(template.New("sms").Parse(smsTemplate))
-    builder := &strings.Builder{}
-    if err := t.Execute(builder, templateData); err != nil {
-      panic(err)
-    }
-    templatedMessageStr := builder.String()
+		// parse the data into the template string:
+		t := template.Must(template.New("sms").Parse(smsTemplate))
+		builder := &strings.Builder{}
+		if err := t.Execute(builder, templateData); err != nil {
+			panic(err)
+		}
+		templatedMessageStr := builder.String()
 
-    // Set the sent-from number to the number I setup in the Twilio dashboard
-    twilioFrom := "+61488811670"
+		// Set the sent-from number to the number I setup in the Twilio dashboard
+		twilioFrom := "+61488811670"
 
-    // Finally, send the message using the go twilio pkg
-    twilio.SendSMS(twilioFrom, smsNumber, templatedMessageStr, "", "")
-    fmt.Println("Sent SMS to ", smsNumber)
+		// Finally, send the message using the go twilio pkg
+		twilio.SendSMS(twilioFrom, smsNumber, templatedMessageStr, "", "")
+		fmt.Println("Sent SMS to ", smsNumber)
 
 	default:
 		log.Printf("Error, unknown event type %s\n", github.WebHookType(r))
@@ -93,9 +93,8 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-  // Start the http service that will accept requests from github
-  log.Println("Server starting on port 8000...")
-  http.HandleFunc("/webhook", handleWebhook)
-  log.Fatal(http.ListenAndServe(":8000", nil))
+	// Start the http service that will accept requests from github
+	log.Println("Server starting on port 8000...")
+	http.HandleFunc("/webhook", handleWebhook)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
-
